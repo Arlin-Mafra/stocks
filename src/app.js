959186cb111100fa950ba160a9db5701b0f1c4b8
Arlin-Mafra <arlinsantana@gmail.com>
2020-09-22 18:10:32 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import "dotenv/config";
 import path from "path";
 import express from "express";
+import cors from "cors";
 import routes from "./routes";
 
 import "./database";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
